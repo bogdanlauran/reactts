9841d3d361962fcb1fb1715b06b13e0db889ee84
Bogdan <lauranbogdan@gmail.com>
2020-07-03 13:51:21 +0300

    sperOK

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Main from './test/clickAlert'
+import Todo from './test/ToDoContainer'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-   <Main />
+   <Todo />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/test/ToDoContainer.js
+++ src/test/ToDoContainer.js
@@ -1,34 +1,19 @@
 import React, { useState } from 'react';
+import CevaList from './ToDoList';
 
-
-function Bleh(props){
+function Todo(){
     const [text, setText] = useState('');
     const [texts, setTexts] = useState([]);
-    
+
     const clicked = () =>{setTexts(texts => [...texts, text])};
 
     return(
         <div>
             <input type="text" id="fname" name="fname" placeholder="Input" onChange={(event)=>setText(event.target.value)}></input>
-            {/* <button type="button" onClick={() => alert(text)}>Click</button> */}
             <button type="button" onClick={clicked}>Click</button>
-            <ul>
-                {texts.map(text=>(
-                    <li>{text}</li> 
-                ))}
-            </ul>
+            <CevaList list={texts}/>
         </div>
     )
 }
 
-
-function Main(){
-
-    return(
-        <div>
-            <Bleh />
-        </div>
-    );
-}
-
-export default Main;
+export default Todo;

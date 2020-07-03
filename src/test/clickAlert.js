c92cf260a5f882299b6e20fc84bb448af14dd47c
Bogdan <lauranbogdan@gmail.com>
2020-07-03 12:25:58 +0300

    iara

--- src/test/clickAlert.js
+++ src/test/clickAlert.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 function Bleh(props){
+    const [text, setText] = useState('');
+    const [texts, setTexts] = useState([]);
+    
+    const clicked = () =>{setTexts(texts => [...texts, text])};
+
     return(
         <div>
-            <input type="text" id="fname" name="fname" placeholder="Input" onChange={(event)=>props={text:event.target.value}}></input>
-            <button type="button" onClick={() => alert(props.text)}>Click</button>
+            <input type="text" id="fname" name="fname" placeholder="Input" onChange={(event)=>setText(event.target.value)}></input>
+            {/* <button type="button" onClick={() => alert(text)}>Click</button> */}
+            <button type="button" onClick={clicked}>Click</button>
+            <ul>
+                {texts.map(text=>(
+                    <li>{text}</li> 
+                ))}
+            </ul>
         </div>
     )
 }

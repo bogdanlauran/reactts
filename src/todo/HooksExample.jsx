1d980a767f05acf646c3040653085a185211891b
Bogdan <lauranbogdan@gmail.com>
2020-12-12 11:26:48 +0200

    UseFetch hook added

--- src/todo/HooksExample.jsx
+++ src/todo/HooksExample.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import useFetch from './UseFetch';
 
 export default function HooksExample() {
-    //const [todo, setTodo] = useState(null);
-    //const [seconds, setSeconds] = useState(0);
+    
 
     const todos = useFetch("https://jsonplaceholder.typicode.com/todos");
 
@@ -14,33 +13,4 @@ export default function HooksExample() {
                 : (todos.todos.map(todo => <pre>{JSON.stringify(todo, null, "\t")}</pre>))}
         </div>
     );
-
-    // fetch('https://jsonplaceholder.typicode.com/todos/1')
-    //     .then(response => response.json())
-    //     .then(todo => {
-    //         console.log('Facut request.');
-    //         setTodo(todo);
-    //     })
-
-    // useEffect(() => {
-    //     (async () => {
-    //         const todo = await fetch('https://jsonplaceholder.typicode.com/todos/1').then(response => response.json());
-    //         console.log('Facut request.', todo);
-    //         setTodo(todo);
-    //     })();
-    // }, []);
-
-    // useEffect(() => {
-    //     // console.log('Este secunda:', seconds);
-
-    //     setTimeout(() => {
-    //         setSeconds(seconds + 1);
-    //     }, 1000);
-    // }, [seconds]);
-
-    // return (
-    //     <>
-
-    //     </>
-    // )
 }
